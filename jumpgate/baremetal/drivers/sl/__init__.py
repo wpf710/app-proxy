3340cdb90fea8da26fc82211a1e14c5e43beeae0
unknown <WANGPENGFEI2@ksc56.kingsoft.cn>
2015-05-18 16:34:17 +0800

    remote sl files

--- jumpgate/baremetal/drivers/sl/__init__.py
+++
@@ -1,9 +0,0 @@
-from jumpgate.baremetal.drivers.sl import nodes
-from jumpgate.common import sl
-
-
-def setup_routes(app, disp):
-    # V1 Routes
-    disp.set_handler('v1_nodes', nodes.NodesV1())
-
-    sl.add_hooks(app)
--- jumpgate/network/drivers/sl/extensions.py
+++
@@ -1,6 +0,0 @@
-
-
-class ExtensionsV2(object):
-    def on_get(self, req, resp):
-        # client = req.env['sl_client']
-        resp.body = {'extensions': []}
